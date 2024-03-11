c5f9f69e73a0535bab2992a65d3679bcfb37ad5f
anthlic <anthony@stukutz.com>
2024-03-11 14:52:02 +1100

    environment variable inclusion

--- src/data/environment_variables.py
+++ src/data/environment_variables.py
@@ -2,5 +2,13 @@ import os
 
 from dotenv import find_dotenv, load_dotenv
 
+(function) def find_dotenv(
+    filename: str = '.env',
+    raise_error_if_not_found: bool = False,
+    usecwd: bool = False
+) -> str
+
 dotenv_path = find_dotenv()
 dotenv_path
+
+load_dotenv(dotenv_path)
