b1d9dd24437dbe231e30fd6b5cdf17c196e0a822
anthlic <hltfinvestments@proton.me>
2024-03-11 14:22:47 +1100

    New Feature

--- src/data/data_prep.py
+++ src/data/data_prep.py
@@ -5,7 +5,8 @@ import matplotlib.pyplot as plt
 c ={'A': [1, 2, 3], 'B': [1,2,3]}
 df= pd.DataFrame(data=c)
 df
+# New feature
 
-d ={'A': [1, 2, 3], 'B': [1,2,3]}
+d ={'A': [1, 2, 3, 4, 5], 'B': [1,2,3,4,5 ]}
 second= pd.DataFrame(data=d)
 second
